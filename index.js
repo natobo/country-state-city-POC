ee81f6b04f6f9dd93f4e34ac9d3024c5e214f588
Nicolas Tobo <nicolastobo@tyba.com.co>
2022-06-13 09:24:56 -0500

    ensayo chile

--- index.js
+++ index.js
@@ -18,5 +18,14 @@ console.log(City.getCitiesOfState('CO','MET'));
 // Separador
 console.log('------------------------CASO CHILE----------------------------------');
 
-// Caso de colombia
+// Caso de Chile
 console.log(Country.getCountryByCode('CL'));
+
+// Caso departamentos de Chile
+console.log(State.getStatesOfCountry('CL'));
+
+// Información departamento del Valparaiso
+console.log(State.getStateByCodeAndCountry('VS','CL'));
+
+// Información de ciudades del departamento del META
+console.log(City.getCitiesOfState('CL','VS'));
